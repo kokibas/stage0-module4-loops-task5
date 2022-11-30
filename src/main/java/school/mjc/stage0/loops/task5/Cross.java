505a79626a553083370d2331b233300fba11634f
kokibas <nurym0807@gmail.com>
2022-11-30 15:49:22 +0600

    10

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -6,7 +6,7 @@ public class Cross {
            for (int i = 0; i < sideLength;i++ ){
                for (int j = 0; j < sideLength; j++ ){
                    if (i == sideLength / 2 || j == sideLength / 2){
-                       System.out.print("*");
+                       System.out.print("8");
                    }
                    else {
                        System.out.print(" ");
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,10 +2,7 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
-
-
-        for ( int i = 1; i <= height; i++) {
+        for ( int i = 1; i <= height - 1; i++) {
 
             for (int k = 1; k < i; k++)
                 System.out.print(" ");
